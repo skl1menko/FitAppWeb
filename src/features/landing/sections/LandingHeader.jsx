2845dbcc20733b5538caae12c62c0f1c25f0d1ec
skl1menko <alklimenko227@gmail.com>
2026-02-10 12:42:30 +0200

    Create adaptive for LandingHeader

--- src/features/landing/sections/LandingHeader.jsx
+++ src/features/landing/sections/LandingHeader.jsx
@@ -1,8 +1,16 @@
 import "./LandingHeader.scss"
 import Logo from '../../../assets/Logo.svg'
 import { Link } from "react-router";
+import { useState } from "react";
 
 const LandingHeader = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+    
     return (
         <div>
             <header>
@@ -11,16 +19,24 @@ const LandingHeader = () => {
                         <img src={Logo} alt="Logo" className="landing-logo-image" />
                         <span>PowerFit</span>
                     </div>
-                    <div className="navbar-links-cont">
-                        <Link>About</Link>
-                        <Link>Features</Link>
-                        <Link>Tracking</Link>
-                        <Link>Analytics</Link>
-                        <Link>Apple Health</Link>
+                    <div className={`navbar-links-cont ${isMenuOpen ? 'active' : ''}`}>
+                        <Link onClick={() => setIsMenuOpen(false)}>About</Link>
+                        <Link onClick={() => setIsMenuOpen(false)}>Features</Link>
+                        <Link onClick={() => setIsMenuOpen(false)}>Tracking</Link>
+                        <Link onClick={() => setIsMenuOpen(false)}>Analytics</Link>
+                        <Link onClick={() => setIsMenuOpen(false)}>Apple Health</Link>
+
+                        <button className="log-in-btn mobile-only">Log In</button>
                     </div>
                     <div className="navbar-btns-cont">
-                        <button className="log-in-btn">Log In</button>
+                        <button className="log-in-btn desktop-only">Log In</button>
                         <button className="get-started-btn">Get Started</button>
+
+                        <button className={`burger-menu ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                        </button>
                     </div>
                 </nav>
             </header>

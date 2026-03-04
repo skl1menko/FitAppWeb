6360ba9229f47574ce6a72177067cd3a0a3ce473
skl1menko <alklimenko227@gmail.com>
2026-03-04 21:17:39 +0200

    add some settings for header

--- src/features/dashboard/DashboardPage.jsx
+++ src/features/dashboard/DashboardPage.jsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from "react"
 import "./DashboardPage.scss"
-
+import WelcomeBoardStat from "./components/DashboardStat"
 const DashboardPage = () => {
+    
+    const [userName, setUserName] = useState('');
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('user'));
+        if (user && user.fullName) {
+            setUserName(user.fullName);
+        }
+    })
+    
     return(
-        <div>
+        <div className="dashboard-main-cont">
+           
         </div>
     )
 }
---
+++ src/features/dashboard/components/DashboardStat.jsx
@@ -0,0 +1,11 @@
+import './DashboardStat.scss'
+
+const DashboardStat = ({color,icon, num, text}) => {
+
+    return(
+        <div className="stat-card-cont">
+           
+        </div>
+    )
+}
+export default WelcomeBoardStat;
